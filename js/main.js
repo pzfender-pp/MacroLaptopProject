b2e9f3d049d2760e4d49974aaf8da0ff0a7e4b2e
pzfender-pp <p.kongjaiman@gmail.com>
2020-09-13 21:16:51 +0700

    addNewJS

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Change navBar when scrolling
+const header = document.querySelector('#head-bar');
+window.addEventListener('scroll', function(){
+  const scrolled = window.scrollY;
+  if (scrolled > 10){
+    header.style.backgroundColor = "#000";
+  }else{
+    header.style.backgroundColor = "rgba(255,255,255,0.2)";
+  }
+});
+
+// Home slide picture
+const slidePic = document.querySelectorAll('#show-case img');
+const currentPic = document.querySelectorAll('.all-current div');
+const currentImg = document.querySelector('#show-case');
+
+let currentPicArr = Array.from(currentPic);
+
+function resetCurrentPic(){
+  let i = 0;
+  for(i = 0; i<currentPicArr.length; i++){
+    currentPicArr[i].classList.remove('currentPicColor');
+    currentImg.children[i].style.display = "none"
+  }
+}
+
+currentPicArr[0].addEventListener('click', function(){
+  resetCurrentPic();
+  currentPicArr[0].classList.add('currentPicColor');
+  currentImg.children[0].style.display = "block";
+});
+currentPicArr[1].addEventListener('click', function(){
+  resetCurrentPic();
+  currentPicArr[1].classList.add('currentPicColor');
+  currentImg.children[1].style.display = "block";
+});
+currentPicArr[2].addEventListener('click', function(){
+  resetCurrentPic();
+  currentPicArr[2].classList.add('currentPicColor');
+  currentImg.children[2].style.display = "block";
+});
+
+
+
